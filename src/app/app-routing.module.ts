8c2d3a618f7fc78eb36b453e333770dbccb854cf
UBALDO17 <christianubaldo78@gmail.com>
2024-11-28 21:20:16 +0800

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tabs/home',  // Default tab
+    pathMatch: 'full',
+  },
+  {
+    path: 'tabs',  // Tabs parent route
+    children: [
+      {
+        path: 'home', // Home tab route
+        loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+      },
+      {
+        path: 'games', // Games tab route
+        loadChildren: () => import('./games/games.module').then(m => m.GamesPageModule),
+      },
+      {
+        path: 'profile', // Profile tab route
+        loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '/tabs/home',  // Fallback route
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
---
+++ src/app/profile/profile.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+})
+export class ProfilePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  editProfile() {
+    console.log('Edit profile clicked!');
+    // Navigate to an edit profile page or open a modal.
+  }
+
+}
